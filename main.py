b9e180d3f9e2a31de49ea8ec5ed5a37b14aba65f
Mary Ann <maryannjose129@gmail.com>
2022-12-03 20:34:43 +0530

    added changes

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-import tkinter as tk
+from tkinter import *
+from PIL import Image,ImageTk
 import requests
-r=requests.get('http://www.omdbapi.com/?i=tt3896198&apikey=cecf960d')
+from urllib.request import urlopen
 
-data = r.json()
+tk=Tk()
+tk.title('Movies')  
+def get_movie():
+    r=requests.get('http://www.omdbapi.com/?i=tt3896198&apikey=cecf960d')
+    data = r.json()
+    image1=data["Poster"]
+    print(image1)
+    u=urlopen(image1)
+    raw_data=u.read()
+    u.close()
+    img=ImageTk.PhotoImage(data=raw_data)
+    label=Label(tk,image=img)
+text_box = Text(tk, height=10, width=50)
+get_button = Button(tk, text="Get Movie", command=get_movie)
+  
+text_box.pack()
+get_button.pack()
+tk.mainloop()
+
